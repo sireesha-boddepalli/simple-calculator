2a1cc8aa3aa44eb4f2ba484b9c56a5fc443704d0
sireesha-boddepalli <boddepallisireesha@gmail.com>
2021-11-01 23:07:26 +0530

    Added keyboard input

--- calculator.js
+++ calculator.js
@@ -16,6 +16,8 @@ function onClick(event){
         let text = resultContainer.innerText;
         resultContainer.innerText = text.substr(0, text.length - 1);
     } else if (id === '=') {
+        // check if expression is non-empty
+        if (resultContainer.innerText === '')   return;
         // try to evaluate the expression
         try {
             // evalute the expression
@@ -36,5 +38,18 @@ function onClick(event){
 for(let i = 0; i < buttons.length; i++){
     let btn = buttons[i];
     btn.addEventListener('click', onClick);
- }
+}
  
+// add key-press listener to 
+document.addEventListener('keydown', (event) => {
+    // check if there is a button corresponding to this key
+    let btn = document.getElementById(event.key);
+    // check if there is such a button
+    if (btn)    btn.click();
+    // check if it's enter
+    else if (event.key === 'Enter')  
+        document.getElementById('=').click();
+    // check if it is backspace
+    else if (event.key === 'Backspace')
+        document.getElementById('DEL').click();
+});
